ef6e52388a1930cb791beefbd29d9fac5de14b12
vishnu-vasan <vasan2001@gmail.com>
2022-05-31 17:48:09 +0530

    modified home page

--- src/components/DonorPost.js
+++ src/components/DonorPost.js
@@ -1,28 +1,239 @@
-import React from "react";
+import React, { useState } from "react";
+import { db } from "../firebaseConfig";
+import { useUserAuth } from "../context/UserAuthContext";
 import { Multiselect } from "multiselect-react-dropdown";
 
-const DonorPost = () => {
-    const donation_category = [
-        { key: "Cooked", category: "Food" },
-        { key: "Raw materials", category: "Food" },
-        { key: "Spices", category: "Food" },
-        { key: "New", category: "Clothes" },
-        { key: "Old", category: "Clothes" },
-        { key: "Electronics", category: "Others" },
-        { key: "Other not in list", category: "Others" }
-        ]
-  return (
-      <>
-       <h2>Select Category</h2>
-      <Multiselect
-        options={donation_category}
-        displayValue="key"
-        groupBy="category"
-        showCheckbox={true}
-      /> 
-        <div>This is the post page</div>
-    </>
-  )
-};
+class DonorPost extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      formValues: [{ category: "", description: "", quantity: "" }]
+    };
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(i, e) {
+    let formValues = this.state.formValues;
+    formValues[i][e.target.name] = e.target.value;
+    this.setState({ formValues });
+  }
+
+  addFormFields() {
+    this.setState(({
+      formValues: [...this.state.formValues, { category: "", description: "", quantity: "" }]
+    }))
+  }
+
+  // let user = useUserAuth();
+
+  removeFormFields(i) {
+    let formValues = this.state.formValues;
+    formValues.splice(i, 1);
+    this.setState({ formValues });
+  }
+  // async pushData() {
+  //   await db.collection("Donations")
+  //     .add({
+  //       category: this.state.formValues.category,
+  //       description: this.state.formValues.description,
+  //       quantity: this.state.formValues.quantity,
+  //     })
+  //     .then((res) => {
+  //       console.log("posted");
+  //     });
+  // }
+
+  handleSubmit(event) {
+    var post = [];
+    event.preventDefault();
+    console.log(this.state.formValues);
+    // for (var i in this.state.formValues) {
+    //   let item = {
+    //     category: this.state.formValues[i].category,
+    //     description: this.state.formValues[i].description,
+    //     quantity: this.state.formValues[i].quantity,
+    //   }
+    //   post.push(item);
+    // }
+    // console.log(post);
+    db.collection("Donations")
+    // for (var i in this.state.formValues) {
+      .push(this.state.formValues)
+      .then((res) => {
+        console.log("posted");
+      });
+    // }
+    alert(JSON.stringify(this.state.formValues));
+    console.log(this.state.formValues);
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        {this.state.formValues.map((element, index) => (
+          <div className="form-inline" key={index}><br/>
+            <label>Category</label>
+            <input type="text" name="category" value={element.category || ""} onChange={e => this.handleChange(index, e)} /><br/>
+            <label>Description</label>
+            <input type="text" name="description" value={element.description || ""} onChange={e => this.handleChange(index, e)} /><br/>
+            <label>Quantity</label>
+            <input type="text" name="quantity" value={element.quantity || ""} onChange={e => this.handleChange(index, e)} /><br/>
+            {
+              index ?
+                <button type="button" className="button remove" onClick={() => this.removeFormFields(index)}>Remove</button>
+                : null
+            }
+          </div>
+        ))}
+        <div className="button-section">
+          <br/>
+          <button className="button add" type="button" onClick={() => this.addFormFields()}>Add</button>{" "}
+          <button className="button submit" type="submit">Submit</button>
+        </div>
+      </form>
+    );
+  }
+}
+
+// const DonorPost = () => {
+//   const donation_category = [
+//     { key: "Cooked", category: "Food" },
+//     { key: "Raw materials", category: "Food" },
+//     { key: "Spices", category: "Food" },
+//     { key: "New clothes", category: "Clothes" },
+//     { key: "Old clothes", category: "Clothes" },
+//     { key: "Electronics", category: "Others" },
+//     { key: "Other not in list", category: "Others" }
+//   ]
+
+//   const [donatingItem, setdonatingItem] = useState("")
+
+//   const onSelect = (selectedList, selectedItem) => {
+//     setdonatingItem(selectedList);
+//     console.log(selectedList);
+//   }
+
+//   const [itemList, setitemList] = useState([{ item: "" }]);
+
+//   const [category, setCategory] = useState("");
+//   const [quantity, setQuantity] = useState("");
+//   const [description, setDescription] = useState("");
+//   const [final, setFinal] = useState("");
+
+//   // const handleitemChange = (e, index) => {
+//   //   const { name, value } = e.target;
+//   //   const list = [...itemList];
+//   //   var item_desc={"category": category, "quantity":quantity, "description":value };
+//   //   console.log(item_desc);
+//   //   list[index][name] = item_desc;
+//   //   // setitemList(list);
+//   //   console.log(name, value);
+//   // };
+
+//   const handleitemRemove = (index) => {
+//     const list = [...itemList];
+//     list.splice(index, 1);
+//     setitemList(list);
+//     setFinal(list);
+//     console.log(list);
+//   };
+
+//   const handleitemAdd = () => {
+//     var item_desc={"category": category, "quantity":quantity, "description":description };
+//     setFinal([...final, item_desc]);
+//     setitemList([...itemList, { item: "" }]);
+//     console.log(final)
+//   };
+
+//   const complete = () => {
+
+//   }
+
+//   return (
+//     <div style={{backgroundColor: "white"}}>
+//        < h2  > Select Category</h2 >
+//       <Multiselect
+//         options={donation_category}
+//         displayValue="key"
+//         groupBy="category"
+//         showCheckbox={true}
+//         onSelect={onSelect}
+//       /> 
+//       {/* <p>{donatingItem}</p> */}
+
+//       <form className="App" autoComplete="off">
+//       <div className="form-field">
+//         <label htmlFor="item">item(s)</label>
+
+//         {itemList.map((singleitem, index) => (
+//           <div key={index} className="items">
+//             <br/>
+//             <div className="first-division">
+//               <input
+//                 name="item"
+//                 type="text"
+//                 id="item"
+//                 // value={singleitem.item}
+//                 onChange={(e) => setCategory(e.target.value)}
+//                 required
+//                 placeholder="Category type"
+//               />
+//               <br/>
+//               <input
+//                 type="text"
+//                 maxLength={10}
+//                 placeholder="Quantity"
+//                 onChange={(e) => setQuantity(e.target.value)}
+//               />
+//               <br/>
+//               <input
+//                 type="text"
+//                 maxLength={100}
+//                 placeholder="Description"
+//                 onChange={(e) => setDescription(e.target.value)}
+//               />
+//               <br/>
+
+//             </div>
+
+//             <div className="second-division">
+//               {itemList.length !== 1 && (
+//                 <button
+//                   type="button"
+//                   onClick={() => handleitemRemove(index)}
+//                   className="remove-btn"
+//                 >
+//                   <span>Remove</span>
+//                 </button>
+//               )}
+//               <br/>
+//             </div>
+//             {itemList.length - 1 === index && (
+//                 <button
+//                   type="button"
+//                   onClick={handleitemAdd}
+//                   className="add-btn"
+//                 >
+//                   <span>Add a item</span>
+//                 </button>
+//               )}
+
+//           </div>
+//         ))}
+//         <button type="button" onClick={complete}>Complete</button>
+//       </div>
+//       {/* <div className="output">
+//         <h2>Output</h2>
+//         {itemList &&
+//           itemList.map((singleitem, index) => (
+//             <ul key={index}>
+//               {singleitem.item && <li>{singleitem.item}</li>}
+//             </ul>
+//           ))}
+//       </div> */}
+//     </form>
+//     </div>
+//   )
+// };
 
 export default DonorPost;
--- src/components/NGOHome.js
+++ src/components/NGOHome.js
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useState, useEffect, useLayoutEffect } from 'react';
 import { Button } from "react-bootstrap";
 import { useNavigate } from "react-router";
 import { useUserAuth } from "../context/UserAuthContext";
 import { Multiselect } from "multiselect-react-dropdown";
-import { useState } from 'react';
+import { db } from "../firebaseConfig";
 
 const NGOHome = () => {
+  var data = [];
+  
+  getDonations();
+  useEffect(() => {
+    let ignore = false;
+
+    if (!ignore) getDonations()
+    return () => { ignore = true; }
+  }, []);
+
   const { logOut, user } = useUserAuth();
+  var data = [{
+    "category": "Food",
+    "description": "Raw food",
+    "id": "j8INRnioVE2AnAzQtuNb",
+    "quantity": "1kg",
+  },
+  {
+    "category": "Clothes",
+    "description": "Shirt and pant",
+    "id": "j8INRnioVE2AnAzQtuNb",
+    "quantity": "4",
+  },
+  ];
+
   const navigate = useNavigate();
   const handleLogout = async () => {
     try {
@@ -16,63 +40,64 @@ const NGOHome = () => {
       console.log(error.message);
     }
   };
-  console.log("ngo homeeee", user.email)
-  // const user_type = [
-  //   {key: "Donor"},
-  //   {key: "NGO"}
-  // ]
 
-  const donation_category = [
-    { key: "Cooked", category: "Food" },
-    { key: "Raw materials", category: "Food" },
-    { key: "Spices", category: "Food" },
-    { key: "New", category: "Clothes" },
-    { key: "Old", category: "Clothes" },
-    { key: "Electronics", category: "Others" },
-    { key: "Other not in list", category: "Others" }
-  ]
-//   const [userType, setuserType] = useState("Donor");
-//   const onSelect = (selectedList, selectedItem) => {
-//         setuserType(selectedItem.key);
-//         console.log(selectedItem);
-// }
+
+  async function getDonations() {
+    await db
+      .collection("Donations")
+      .get()
+      .then((res) => {
+        var db_data = res.docs[0].data(0);
+        for (var i in db_data) {
+          console.log("Res:\n", db_data[i]);
+          db_data[i]["id"] = i;
+          data.push(db_data[i]);
+          //setdata([...data, db_data[i]]);
+        }
+        console.log("Res:\n", data);
+      })
+  }
+
+  function Card(props) {
+    console.log(props.itemData);
+    return (
+      <div>
+        <div className="p-01 box mt-3 text-center" >
+          <h2 >{props.itemData.category}</h2>
+          <p >{props.itemData.description}</p>
+          <p >{props.itemData.quantity}</p>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <>
+      <h1>Available Donations</h1>
+      <div>
+        {data.map((item) => {
+          return <Card itemData={item} />
+        })}
+      </div>
+      <br />
       <div className="d-grid gap-2">
         <Button variant="primary" onClick={handleLogout}>
           Log out
         </Button>
       </div>
-      <br/>
+      <br />
       <div className="p-4 box mt-3 text-center">
         <b>Welcome NGO<br />
-        {user && user.email}</b>
+          {user && user.email}</b>
       </div>
-      <br/>
-      {/* <h4>Select Type of User</h4>
-      <Multiselect
-        options={user_type}
-        singleSelect
-        displayValue="key"
-        onSelect={onSelect}
-      />
-      <br/> */}
+      <br />
       <div className="d-grid gap-2">
-        <Button variant="primary" >
+        <Button variant="primary" onClick={getDonations}>
           Continue
         </Button>
       </div>
-      <br/><br/>
-      
-      
-      {/* <h2>Select Category</h2>
-      <Multiselect
-        options={donation_category}
-        displayValue="key"
-        groupBy="category"
-        showCheckbox={true}
-      /> */}
-      <br/><br/>
+      <br /><br />
+      <br /><br />
     </>
   );
 };
